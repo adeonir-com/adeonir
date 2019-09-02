b35513bd3662fccffce5b698b3ce9fb89c654e10
Adeonir Kohl <adeonir@gmail.com>
2019-09-01 22:39:12 -0300

    Update sizes

--- src/components/Avatar/styles.js
+++ src/components/Avatar/styles.js
@@ -4,9 +4,7 @@ import Img from "gatsby-image"
 
 export const AvatarImage = styled(Img)`
   margin: 1rem;
-  height: 8rem;
-  width: 8rem;
   border-radius: 50%;
-  box-shadow: 0 0 0 0.5rem ${rgba("#f8f9fa", 0.3)},
-    0 0 0 1rem ${rgba("#f8f9fa", 0.3)};
+  box-shadow: 0 0 0 0.5rem ${rgba("#f8f9fa", 0.2)},
+    0 0 0 1rem ${rgba("#f8f9fa", 0.2)};
 `
