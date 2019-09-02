f1b01e58766f2e8f837efcc56925246d5cedbb4d
Adeonir Kohl <adeonir@gmail.com>
2019-09-01 22:05:37 -0300

    Add sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import Profile from "../Profile"
+import Social from "../Social"
+
+import * as S from "./styles"
+
+const Sidebar = () => <S.SidebarWrapper></S.SidebarWrapper>
+
+export default Sidebar
---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const SidebarWrapper = styled.aside`
+  align-items: center;
+  background: #282a36;
+  border-right: 1px solid #44475a;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  padding: 2rem;
+  position: fixed;
+  text-align: center;
+  width: 20rem;
+`
