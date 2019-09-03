e4dd9789ac14636a78f0838db3ad6c39d9426db2
Adeonir Kohl <adeonir@gmail.com>
2019-09-03 00:18:58 -0300

    Add postitem component

---
+++ src/components/PostItem/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+export const PostItemWrapper = styled.section``
+
+export const PostItemLink = styled(Link)``
+
+export const PostItemTag = styled.div``
+
+export const PostItemInfo = styled.div``
+
+export const PostItemDate = styled.time``
+
+export const PostItemTitle = styled.h1``
+
+export const PostItemTitle = styled.p``
