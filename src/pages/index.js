ae6000fe52e7449016f3fa6ea80572b175a38cfc
Adeonir Kohl <adeonir@gmail.com>
2019-09-29 15:02:21 -0300

    Fix sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import Seo from '../components/Seo'
 
 import * as S from '../styles/pages'
 
-import '../../node_modules/overlayscrollbars/css/OverlayScrollbars.css'
-
 const HomePage = () => (
   <Layout>
     <Seo title='Home' />
