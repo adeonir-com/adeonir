8c6157ee45c71665eb2ec99ee2eb86991db0d247
Adeonir Kohl <adeonir@gmail.com>
2020-01-11 13:00:04 -0300

    Add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,8 +75,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Adeonir Kohl',
+        short_name: 'Adeonir Kohl',
         start_url: '/',
         background_color: '#252a41',
         theme_color: '#252a41',
@@ -87,6 +87,7 @@ module.exports = {
     'gatsby-plugin-eslint',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify-cms',
   ],
 }
